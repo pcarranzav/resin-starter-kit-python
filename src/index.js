85e532f7a50f3fdaee222c904776d4dd59e347a7
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-04-08 14:21:20 -0300

    Added express server. Will dropbear work with -F?

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+var app = require('express')();
 console.log('Hello world!');
+
+app.get('/', function (req, res) {
+	res.send('Hello World!');
+});
+
+var server = app.listen(80, function(){
+	console.log('Server running on port 80.');
+})
